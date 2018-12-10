a20178c001ff493de468e20b9eaa497fe858d53d
Samik Saha <samiksaha88@gmail.com>
2018-12-10 19:28:30 +0530

    added junit testing framework

---
+++ test/com/samiksaha/infa/automateds2t/S2TGeneratorTest.java
@@ -0,0 +1,19 @@
+package com.samiksaha.infa.automateds2t;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class S2TGeneratorTest {
+
+	@Test
+	public void testDoInBackground() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOutputToExcel() {
+		fail("Not yet implemented");
+	}
+
+}
